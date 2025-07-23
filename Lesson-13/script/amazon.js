9bfa19f85e5f6ebcb2e551f661688431b0596346
Sadiqur Rahman <62737129+sadiqur-rahman@users.noreply.github.com>
2025-07-23 16:20:56 +0600

    Update cart quantity display dynamically
    Replaces the hardcoded cart quantity in amazon.html with a dynamic value. Updates amazon.js to calculate and display the total cart quantity whenever an item is added to the cart.

--- Lesson-13/script/amazon.js
+++ Lesson-13/script/amazon.js
@@ -76,7 +76,18 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
       }
+      
+      // finding total quantity
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
+      
+      console.log(cartQuantity);
       console.log(cart);
+
+      document.querySelector('.cart-quantity').innerHTML = cartQuantity;
     })
   })
 
